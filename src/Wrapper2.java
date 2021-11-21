4452a9693cd3e4c4416db5280c20ce70c1194f99
Amit <amit.kumar_cs20@gla.ac.in>
2021-11-21 22:19:04 +0530

    Initial commit

---
+++ src/Wrapper2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Wrapper2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Give the Number:\n");
+
+//        Using Command line argument and store the wrapper Class
+        Integer a= sc.nextInt();
+//        Print the Values
+        System.out.println("Binary Equivalant: "+Integer.toBinaryString(a));
+        System.out.println("\nHexa Equivalant: "+Integer.toHexString(a));
+        System.out.println("\nOctal Equivalant: "+Integer.toOctalString(a));
+
+    }
+}
